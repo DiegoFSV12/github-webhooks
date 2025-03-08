6ae220565ead58fd28d9466706f68ddc57822647
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-03-07 19:53:12 -0500

    GitHubController

--- src/presentation/github/controller.ts
+++ src/presentation/github/controller.ts
@@ -1,15 +1,24 @@
 import { Request, Response } from "express";
+import { GitHubService } from "../services/github.service";
 
 export class GithubController{
-    constructor(){}
+    constructor(
+        private readonly githubService = new GitHubService(),
+    ){}
 
     webhookHandler=(req:Request,res:Response)=>{
-        const githubEvent = req.header('x-github-event') ?? 'unknown';
+        const githubEvent = req.header('x-github-event') ?? 'unknown';//Devuelve el evento que se deo en Github
         //const signature = req.header('x-gub-signature-256') ?? 'unknown';
         const payload = req.body;
-        
-        
-
+        let message:string;
+        switch(githubEvent){
+            case 'star':
+                message = this.githubService.onStar(payload);
+                break;
+            default:
+                message = `Unknown evente => ${githubEvent}`;
+        }
+        console.log(message);
         res.status(202).send('Accepted');
     }
 }
