f5fe2f17431cc9bc4395c740e385b3337607e18c
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-03-10 15:14:22 -0500

    controller.ts

--- src/presentation/github/controller.ts
+++ src/presentation/github/controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { GitHubService } from "../services/github.service";
 import { DiscordService } from "../services/discord.service";
+import { images } from "../../assets";
 
 export class GithubController{
     constructor(
@@ -16,15 +17,21 @@ export class GithubController{
         switch(githubEvent){
             case 'star':
                 message = this.githubService.onStar(payload);
+                this.sendNotify(message,images.star,res);
                 break;
             case 'issues':
                 message = this.githubService.onIssue(payload);
+                this.sendNotify(message,images.issue,res);
                 break;
             default:
                 message = `Unknown event => ${githubEvent}`;
+                this.sendNotify(message,images.unknown,res);
         }
         console.log(message);
-        this.discordService.notify(message)
+    }
+
+    sendNotify(message:string,type:string,res:Response){
+        this.discordService.notify(message,type)
             .then(()=>res.status(202).send('Accepted'))
             .catch(()=>res.status(500).json({error:'Internal server error'}));
     }
