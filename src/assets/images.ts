7f58655e923b3de12d29f2517da00694672831f4
DiegoFSV12 <dsanchezver@ucvvirtual.edu.pe>
2025-03-10 15:14:11 -0500

    image.ts

---
+++ src/assets/images.ts
@@ -0,0 +1,6 @@
+export const images = {
+    star: 'https://www.gifcen.com/wp-content/uploads/2021/11/hu-tao-16.gif',
+    issue: 'https://i.pinimg.com/originals/d9/ab/80/d9ab80f638411facf70ab1ad542ff380.gif',
+    unknown: 'https://i.pinimg.com/originals/1f/a1/cd/1fa1cd70056af89ce02690a69da1ec60.gif',
+};
+
